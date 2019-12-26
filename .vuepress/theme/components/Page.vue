7b4207271427ec047225cd96caaa59b38c7e13b0
shfshanyue <xianger94@gmail.com>
2019-12-26 08:40:24 +0800

    update

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -79,7 +79,7 @@ export default {
   },
   computed: {
     isLock () {
-      return this.lock ? Math.random() > 0.1 : false
+      return this.lock ? Math.random() > 0.2 : false
       /* return false */
     }
   },
